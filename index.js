72dddde5ebabda8984fa653b6bc99753af432997
manu <164546874+emanuelmsz@users.noreply.github.com>
2024-12-07 00:33:39 +0000

    atividade 15

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv"; 
-import {selectUsuarios} from "./bd.js";   
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";   
 
 dotenv.config();
 
 const app = express();          
-const port = 3000;                 
+const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {      
   console.log("Rota GET/ solicitada");
@@ -23,6 +25,44 @@ app.get("/usuarios", async (req, res) => {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
   console.log("Rota GET/usuarios solicitada");
+});
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.delete("/usuario/:id", async (req, res) => {
+  console.log("Rota DELETE /usuario/# solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) {
+      await deleteUsuario(req.params.id);
+      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.put("/usuario/:id", async (req, res) => {
+  console.log("Rota PUT /usuario/# solicitada");
+  try {
+    const id = req.params.id;
+    const usuario = await selectUsuario(id);
+    if (usuario.length > 0) {
+      await updateUsuario(id, req.body);
+      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    console.log(error);
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
 app.listen(port, () => {       
