dfa688e61893648a48050401154baa4ed31820b9
manu <164546874+emanuelmsz@users.noreply.github.com>
2025-02-14 23:21:40 +0000

    a

--- src/index.js
+++ src/index.js
@@ -1,68 +1,23 @@
-import { Router } from "express";
+import dotenv from "dotenv";
+import express from "express";
+import roteadorUsuario from "./routes/usuario.js";
 
-import {
-  selectUsuario,
-  selectUsuarios,
-  insertUsuario,
-  deleteUsuario,
-  updateUsuario,
-} from "../db/index.js";
 
-const router = Router();
+app.use(roteadorUsuario);
+dotenv.config();
 
-router.get("/usuario", async (req, res) => {
-  console.log("Rota GET /usuario solicitada");
-  try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API para CRUD usuario: link_gitgub",
+  });
 });
 
-router.get("/usuario/:id", async (req, res) => {
-  console.log(`Rota GET /usuario/${req.params.id} solicitada`);
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) res.json(usuario);
-    else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
 });
-
-router.post("/usuario", async (req, res) => {
-  console.log("Rota POST /usuario solicitada");
-  try {
-    await insertUsuario(req.body);
-    res.status(201).json({ message: "Usuário inserido com sucesso!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-router.put("/usuario", async (req, res) => {
-  console.log("Rota PUT /usuario solicitada");
-  try {
-    const usuario = await selectUsuario(req.body.id);
-    if (usuario.length > 0) {
-      await updateUsuario(req.body);
-      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
-    } else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    console.log(error);
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-router.delete("/usuario/:id", async (req, res) => {
-  console.log("Rota DELETE /usuario solicitada");
-  try {
-    await deleteUsuario(req.params.id);
-    res.status(200).json({ message: "Usuário excluido com sucesso!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-export default router;
