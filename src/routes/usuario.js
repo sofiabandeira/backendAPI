ccaf101854ca790216f6044bb5bba0c800dfee3d
manu <164546874+emanuelmsz@users.noreply.github.com>
2025-02-15 02:01:19 +0000

    a

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,6 +1,4 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
-import { autenticarUsuario } from "../db/index.js";
 
 import {
   selectUsuario,
@@ -67,19 +65,5 @@ router.delete("/usuario/:id", async (req, res) => {
   }
 });
 
-router.post("/login", async (req, res) => {
-  console.log("Rota POST /login solicitada");
-  try {
-    const usuario = await autenticarUsuario(req.body.email, req.body.senha);
-    if (usuario !== undefined) {
-      const token = jwt.sign({ user: usuario.id, acesso: usuario.acesso }, process.env.SECRET, {
-        expiresIn: 30000,
-      });
-      res.status(202).json({ token: token });
-    } else res.status(404).json({ message: "Usuário/Senha incorreta!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
 
 export default router;
